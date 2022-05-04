e0cf75a6d00552e32621e4bef895edb71490cc87
Chris Karpyszyn <ckarpyszyn@tractionrec.com>
2022-05-04 09:13:55 -0400

    move scripts into folder and add stackable tag to objects

---
+++ Assets/Scripts/Stackable.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stackable : MonoBehaviour
+{
+    List<GameObject> stackedObjects = new List<GameObject>();
+    private int count = 0;
+    public string countString;
+
+    
+
+}
